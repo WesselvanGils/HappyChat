1f7964f9ec663b12d009e10bc36474fa185525c6
TheElementalDragon <60363527+TheElementalDragon@users.noreply.github.com>
2021-05-28 13:17:07 +0200

    video and audio will now be visible

--- public/script.js
+++ public/script.js
@@ -1,14 +1,42 @@
 const socket = io("/")
-const myPeer = new Peer(undefined, 
+const videoGrid = document.getElementById("video-grid")
+const myVideo = document.createElement("video")
+const myPeer = new Peer(undefined,
     {
         host: "/",
         port: "3001"
     }
 )
 
-socket.emit("join-room", ROOM_ID, 10)
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia(
+    {
+        video: true,
+        audio: true
+    }
+).then(stream =>
+    {
+        addVideoStream(myVideo, stream)
+    }
+)
+
+myPeer.on("open", id =>
+{
+    socket.emit("join-room", ROOM_ID, id)
+})
 
 socket.on("user-connected", userId =>
 {
     console.log(`User connected ${userId}`)
 })
+
+function addVideoStream(video, stream)
+{
+    video.srcObject = stream
+    video.eventListener("loadedmetadata", () =>
+    {
+        video.play()
+    })
+    videoGrid.append(video)
+}
