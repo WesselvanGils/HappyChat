dd053cf8d3484052ccd5b5987839fe6b3531cc45
TheElementalDragon <60363527+TheElementalDragon@users.noreply.github.com>
2021-06-07 11:28:30 +0200

    Now accounts for uneven numbers

--- roulette.js
+++ roulette.js
@@ -6,17 +6,34 @@ const males = participants.filter(participant => participant.gender == "male")
 
 let matches = []
 
-males.forEach(maleParticipant =>
+let timeOfDate = 0
+
+males.forEach(function callback(maleParticipant, index) 
 {
-    let timeOfDate = 0
+    console.log(index)
+    const dateLength = 15
 
     females.forEach(femaleParticipant =>
     {
-        matches.push({ match: [ maleParticipant.email, femaleParticipant.email ], time: timeOfDate, link:"" })
-        timeOfDate += 15
+        matches.push({ match: [ maleParticipant.email, femaleParticipant.email ], time: timeOfDate, link: "" })
+        timeOfDate += dateLength
     })
 
     females.push(females.shift())
+
+    if (males.length != females.length)
+    {
+        let difference = males.length - females.length
+        if (difference < 0) { difference *= -1}
+
+        if (index == difference) 
+        {
+            timeOfDate = difference * dateLength * 2
+        }else
+        {
+            timeOfDate = 0
+        }
+    }
 })
 
 matches.forEach(match =>
