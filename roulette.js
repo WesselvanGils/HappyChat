fa93c1215d7bb19f5ca6e5b6a6c0a376abf0ddaa
TheElementalDragon <60363527+TheElementalDragon@users.noreply.github.com>
2021-06-07 10:49:55 +0200

    matches are now scheduled

--- roulette.js
+++ roulette.js
@@ -5,12 +5,22 @@ const females = participants.filter(participant => participant.gender == "female
 const males = participants.filter(participant => participant.gender == "male")
 
 let matches = []
+let timeOfDate = 0
 
 males.forEach(maleParticipant =>
 {
+    timeOfDate = 0
+
     females.forEach(femaleParticipant =>
     {
-        matches.push({ match: [ maleParticipant, femaleParticipant] })
-        console.log(`${maleParticipant.firstName} matched with ${femaleParticipant.firstName}`)
-    });
+        matches.push({ match: [ maleParticipant, femaleParticipant ], time: timeOfDate, link:"" })
+        timeOfDate += 15
+    })
+
+    females.push(females.shift())
+})
+
+matches.forEach(match =>
+{
+    console.log(match)
 });
